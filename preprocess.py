92873f98b1dbfd9820bd5c89e12fc60765d07c04
Snow59 <82878319+Snow59@users.noreply.github.com>
2024-06-07 23:34:35 +0200

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,32 +1,27 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+import matplotlib.pyplot as plt
 
 def load_data(filepath):
-    """Importe les données depuis un fichier CSV."""
     df = pd.read_csv(filepath)
-    print("Premières lignes des données importées :")
-    print(df.head())
-    print("\nInformations sur les données :")
     print(df.info())
+    print(df.head())
     return df
 
 def examine_data(df):
-    """Examine les données pour en comprendre la structure."""
-    print("\nTaille du jeu de données :", df.shape)
-    print("\nTypes des données :")
-    print(df.dtypes)
-    print("\nValeurs manquantes par colonne :")
-    print(df.isna().sum())
-    print("\nDescription des données :")
-    print(df.describe())
-    df.hist(figsize=(20, 15))
+    print("Taille du jeu de données:", df.shape)
+    print("Types de données:\n", df.dtypes)
+    print("Valeurs manquantes:\n", df.isna().sum())
+
+    # Visualiser la distribution des données
+    df.hist(bins=30, figsize=(20, 15))
     plt.show()
 
 def preprocess_data(df):
-    """Prépare les données pour les algorithmes de classification."""
     # Séparer les variables en numériques et catégorielles
     numerical_cols = df.select_dtypes(include=['int64', 'float64']).columns.tolist()
     categorical_cols = df.select_dtypes(include=['object']).columns.tolist()
@@ -52,11 +47,9 @@ def preprocess_data(df):
 
     # Préparation des données
     df_processed = preprocessor.fit_transform(df)
-    df_processed = pd.DataFrame(df_processed)  # Conversion en DataFrame
-    return df_processed
+    return pd.DataFrame(df_processed)
 
 def save_processed_data(df, filename):
-    """Enregistre les données prétraitées dans un fichier CSV."""
     df.to_csv(filename, index=False)
 
 if __name__ == "__main__":
@@ -64,10 +57,8 @@ if __name__ == "__main__":
     data_path = 'car_insurance.csv'
     processed_data_path = 'processed_car_insurance.csv'
 
-    # Charger les données
+    # Charger et examiner les données
     data = load_data(data_path)
-
-    # Examiner les données
     examine_data(data)
 
     # Préparer les données
